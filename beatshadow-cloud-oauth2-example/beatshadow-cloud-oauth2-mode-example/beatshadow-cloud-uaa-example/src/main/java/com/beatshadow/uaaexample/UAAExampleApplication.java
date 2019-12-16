0422987336f0d2ed93751f48bf7f9df264f00188
gnehcgnaw <gnehcgnaw@gamil.com>
2019-12-17 01:30:46 +0800

    1.创建UAA授权服务工程； 2.创建Order-Service资源服务工程。

---
+++ beatshadow-cloud-oauth2-example/beatshadow-cloud-oauth2-mode-example/beatshadow-cloud-uaa-example/src/main/java/com/beatshadow/uaaexample/UAAExampleApplication.java
@@ -0,0 +1,13 @@
+package com.beatshadow.uaaexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UAAExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UAAExampleApplication.class, args);
+	}
+
+}
